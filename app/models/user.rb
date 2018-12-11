9a6c4fda7fa7084eb03a0b0473ee9a5da75620a0
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-11 15:28:12 -0800

    Complete User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  zipcode         :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :first_name, :last_name, :zipcode, :session_token, :password_digest, presence: true
+  validates :email, presence: true, uniqueness: true
+  
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return nil unless user
+    user.is_password?(password) ? user : nil
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+end
