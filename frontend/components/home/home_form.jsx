fd99f3bbcecfb5ba9e0429e6e0e4ecfe899c14f7
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-13 21:58:48 -0800

    Update styles on homepage and session forms

--- frontend/components/home/home_form.jsx
+++ frontend/components/home/home_form.jsx
@@ -17,49 +17,43 @@ class HomeForm extends React.Component{
     if(this.props.currentUser){
       return (
         <form onSubmit={this.handleSubmit} className="nav-bar-right">
-          <input type="submit" value="Logout" />
+          <input type="submit" value="Logout" className="home-underline"/>
         </form>
       )
     } else {
       return (
         <div className="nav-bar-right">
-          <Link to='/login'>Demo User</Link>
-          <Link to='/login' onClick={this.props.clearErrors}>Log in</Link>
-          <Link to='/signup' onClick={this.props.clearErrors}>Sign Up</Link>
+          <Link to='/login' className="home-underline">Demo User</Link>
+          <Link to='/login' onClick={this.props.clearErrors} className="home-underline">Log in</Link>
+          <Link to='/signup' onClick={this.props.clearErrors} className="home-signup">Sign Up</Link>
         </div>
       )
     }
   }
 
   render(){
-    return (
-      <div className="home-page">
+    return <div className="home-page">
         <div className="home-main-section">
           <div className="home-main-nav-bar">
             <div className="home-nav-bar">
               <div className="nav-bar-left">
-                Write a Review
+                <div className="home-underline">Write a Review</div>
+                <a href="https://github.com/JPArriola" className="github"></a>
+                <a href="https://www.linkedin.com/in/joshuaarriola/" className="linkedin"></a>
               </div>
-              <div >
-                {this.loggedIn()}
-              </div>
+              <div>{this.loggedIn()}</div>
             </div>
           </div>
           <div className="home-main-inner">
             <div className="home-logo-banner">
-              <div className="home-logo">
-              </div>
+              <div className="home-logo" />
             </div>
             <div className="home-search-bar-main">
-              <div className="home-search-bar">
-
-              </div>
+              <div className="home-search-bar" />
             </div>
           </div>
-
         </div>
-      </div>
-    )
+      </div>;
   }
 }
 
