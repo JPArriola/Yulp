80ab1dd454a31b1eec951f69475f96610d3ec4d6
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-16 13:56:09 -0800

    Finish seeding on boba, coffee, and bar categories

--- frontend/components/business/business_index_item.jsx
+++ frontend/components/business/business_index_item.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BusinessIndexItem = props => (
-  <li>
-    
-    <Link to={`/businesses/${props.business.id}`}>{props.business.biz_name}</Link>
+  <li> 
+    <Link to={`/businesses/${props.business.id}`}>{props.business.bizName}</Link>
   </li>
 );
 
