a900afbd21d3bb702c93aa017e6930d1f16c6e32
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-18 11:01:28 -0800

    Finish skeleton build of business showpage

--- frontend/components/business/business_show.jsx
+++ frontend/components/business/business_show.jsx
@@ -15,40 +15,67 @@ class BusinessShow extends React.Component{
     return <div>
         <Navbar />
         <div className="showpage-master-size">
-          <div className="showpage-working-size">
-            <div className="showpage-business-info-master">
-              <div className="showpage-business-info-top-master">
-                <div className="showpage-business-info-top-left">
-                  <div className="showpage-business-name">
-                    {biz.bizName}
+          <div className="showpage-working-master-size">
+            <div className="showpage-working-size">
+              <div className="showpage-business-info-master">
+                <div className="showpage-business-info-top-master">
+                  <div className="showpage-business-info-top-left">
+                    <div className="showpage-business-name">
+                      {biz.bizName}
+                    </div>
+                    <div className="showpage-business-rating">
+                      {biz.rating}
+                    </div>
+                    <div className="showpage-business-price">
+                      {biz.price}
+                    </div>
                   </div>
-                  <div className="showpage-business-rating">
-                    {biz.rating}
+                  <div className="showpage-business-info-top-right">
+                    <div className="showpage-review-button-container">
+                      <i className="fas fa-star"> Write a Review</i>
+                    </div>
                   </div>
-                  <div className="showpage-business-price">{biz.price}</div>
                 </div>
-                <div className="showpage-business-info-top-right">
-                  <div className="showpage-review-button-container">
-                    <i class="fas fa-star"> Write a Review</i>
+                <div className="showpage-business-info-lower-master">
+                  <div className="showpage-business-info-lower-parent">
+                    <div className="showpage-map-container">
+                      <div className="showpage-map" />
+                    </div>
+                    <div className="showpage-info-photos">
+                      <div className="showpage-info-side-photo">
+                        side photo
+                      </div>
+                      <div className="showpage-info-middle-photo">
+                        middle photo
+                      </div>
+                      <div className="showpage-info-side-photo">
+                        side photo
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="showpage-business-info-lower-master">
-                <div className="showpage-business-info-lower-parent">
-                  <div className="showpage-map-container">
-                    <div className="showpage-map">
-                      
-                    </div>
-                  </div>
-                  <div className="showpage-info-photos">
-                    <div className="showpage-info-side-photo">side photo</div>
-                    <div className="showpage-info-middle-photo">middle photo</div>
-                    <div className="showpage-info-side-photo">side photo</div>
-                  </div>
-                </div>
-              </div>                
             </div>
           </div>
+            <div className="showpage-working-size">
+              <div className="showpage-business-content-master">
+                <div className="showpage-business-content-parent" >
+                  reviews
+                </div>
+                <div className="showpage-business-sidebar-parent" >
+                  <div className="showpage-business-sidebar-top-info">
+                    hours / price
+                  </div>
+                  <div className="showpage-business-sidebar-hours">
+                    hours
+                  </div>
+                  <div className="showpage-business-sidebar-info">
+                    business info
+                  </div>
+                </div>
+              </div>
+            </div>
+
         </div>
       </div>;
   }
