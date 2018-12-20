dfe3551a944579bca3efb759e1c96bf9eaca87ec
Joshua Arriola <joshuapatrickarriola@gmail.com>
2018-12-20 15:16:12 -0800

    Create container for business show page sidebar business info section

--- frontend/components/business/business_show_hours_container.js
+++ frontend/components/business/business_show_hours_container.js
@@ -3,9 +3,9 @@ import BusinessShowHours from './business_show_hours';
 import { withRouter } from "react-router-dom";
 
 const mSP = (state, ownProps) => {
-  console.warn(ownProps);
   let bizId = ownProps.match.params.id;
   let hours = state.entities.businesses[bizId].hours;
+  console.warn(state.entities.businesses[bizId]);
   return ({
     hours,
   });
---
+++ frontend/components/business/business_show_info_container.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import BusinessShowInfo from './business_show_hours';
+import { withRouter } from "react-router-dom";
+
+const mSP = (state, ownProps) => {
+  let bizId = ownProps.match.params.id;
+  let business = state.entities.businesses[bizId];
+  return ({
+    business,
+  });
+};
+
+
+export default withRouter(connect(mSP, null)(BusinessShowInfo));
