4c9b1c96c6378220eb6cd1dc025cd0d0226e1bf6
Joshua Arriola <joshuapatrickarriola@gmail.com>
2019-01-24 15:08:54 -0800

    Finish homepage categories section

--- frontend/components/home/home_form.jsx
+++ frontend/components/home/home_form.jsx
@@ -48,38 +48,70 @@ class HomeForm extends React.Component{
   }
 
   render(){
-    return <div className="home-page">
-        <div className="home-main-section">
-          <div className="home-main-nav-bar">
-            <div className="home-nav-bar">
-              <div className="nav-bar-left">
-                <div className="home-underline">Write a Review</div>
-                <a href="https://github.com/JPArriola" target="_blank" className="github" />
-                <a href="https://www.linkedin.com/in/joshuaarriola/" target="_blank" className="linkedin" />
+    return <div>
+        <div className="home-page">
+          <div className="home-main-section">
+            <div className="home-main-nav-bar">
+              <div className="home-nav-bar">
+                <div className="nav-bar-left">
+                  <div className="home-underline">Write a Review</div>
+                  <a href="https://github.com/JPArriola" target="_blank" className="github" />
+                  <a href="https://www.linkedin.com/in/joshuaarriola/" target="_blank" className="linkedin" />
+                </div>
+                <div className="nav-right-parent">{this.loggedIn()}</div>
               </div>
-              <div className="nav-right-parent">{this.loggedIn()}</div>
+            </div>
+            <div className="home-main-inner">
+              <div className="home-logo" />
+              <div className="home-search-bar">
+                <div className="home-search-find">
+                  <div className="home-search-find-title">Find</div>
+                  <input className="business-search-find" type="text" placeholder="coffee, boba, beer" />
+                </div>
+                <div className="home-search-loc">
+                  <div className="home-search-loc-input">
+                    <div className="home-search-find-near">Near</div>
+                    <input className="business-search-near" type="text" placeholder="City" defaultValue="San Francisco" />
+                  </div>
+                  <button className="home-search-submit">
+                    <i className="fas fa-search" />
+                  </button>
+                </div>
+              </div>
+              <div className="home-category-navigation-bar">
+                <Link to="/businesses" className="nav-underline">
+                  Businesses
+                </Link>
+                <div className="home-category-navigation-business-boba" />
+                <div className="home-category-navigation-business-coffee" />
+                <div className="home-category-navigation-business-bars" />
+              </div>
             </div>
           </div>
-          <div className="home-main-inner">
-            <div className="home-logo" />
-            <div className="home-search-bar">
-              <div className="home-search-find">
-                <div className="home-search-find-title">Find</div>
-                <input className="business-search-find" type="text" placeholder="coffee, boba, beer" />
+        </div>
+        <div className="home-page-lower">
+          <div className="home-lower-title">Your Next Drink Awaits</div>
+          <div className="home-lower-categories-container">
+            <div className="home-drink-category" >
+              <div className="home-coffee-picture">
               </div>
-              <div className="home-search-loc">
-                <div className="home-search-find-title">Near</div>
-                <input className="business-search-near" type="text" placeholder="City" />
-                <button className="home-search-submit">
-                  <i className="fas fa-search"></i>
-                </button>
+              <div className="home-category-text">
+                Coffee
               </div>
             </div>
-            <div className="home-category-navigation-bar">
-              <Link to='/businesses' className="nav-underline">Businesses</Link>
-              <div className="home-category-navigation-business-boba" />
-              <div className="home-category-navigation-business-coffee" />
-              <div className="home-category-navigation-business-bars" />
+            <div className="home-drink-category" >
+              <div className="home-boba-picture">
+              </div>
+              <div className="home-category-text">
+                Boba
+              </div>
+            </div>
+            <div className="home-drink-category" >
+              <div className="home-beer-picture">
+              </div>
+              <div className="home-category-text">
+                Beer
+              </div>
             </div>
           </div>
         </div>
